04e5cde8d970cc1c7da9ee4e95b30f66c64f98ca
gp-kobayashi <kobayashi_y@gp1000.jp>
2024-09-09 15:21:49 +0900

    [wip]messageの型定義について

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,40 @@
-import Image from "next/image";
+"use client"; 
+
 import styles from "./page.module.css";
+import io from "socket.io-client";
+import { useState } from "react";
+
+const socket = io("http://localhost:5000")
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+  const [message, setMessage] = useState("");
+  const [list, setList] = useState<string[]>([]);
+
+  const handleSendMessage = () =>{
+    socket.emit("send_message",{message: message});
+    setMessage("");
+  };
+
+  socket.on("received_message",(data: string) =>{
+    setList([...list, data]);
+  });
+
+  return (
+    <div className={styles.container}>
+      <h2>チャットアプリ</h2>
+      <div className={styles.chatInputButton}>
+        <input type="text"
+        placeholder="チャット…"
+        onChange={(e) => setMessage(e.target.value)}
+        value={message}/>
+        <button onClick={() => handleSendMessage()}>チャット送信</button>
+      </div>
+      {list.map((chat) =>(
+        <div className={styles.chatArea} key={chat.message}>
+          {chat.message}
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      ))}
     </div>
   );
 }
